fff2706ba1dfda6fd2a29784696d4f0ec2834440
eukkbi6 <dhtmddn0324@gmail.com>
2022-02-08 20:04:50 +0900

    Third step

---
+++ src/eukkbi/core/Main.java
@@ -0,0 +1,26 @@
+package eukkbi.core;
+
+import arc.Events;
+import eukkbi.content.ModLoader;
+import mindustry.game.EventType;
+import mindustry.mod.Mod;
+
+public class Main extends Mod {
+    public Main() {
+        Events.on(EventType.ClientLoadEvent.class, e -> {
+
+        });
+
+        Events.on(EventType.WorldLoadEvent.class, e -> {
+
+        });
+    }
+    @Override
+    public void init(){
+    }
+
+    @Override
+    public void loadContent(){
+        new ModLoader().load();
+    }
+}
